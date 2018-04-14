db34233c1b4f13588c31f3f768cdfd77006f9e25
Jorge Villalobos <jorgemvc@gmail.com>
2018-04-13 21:57:55 -0600

    Create Taller-Fade.ino

---
+++ Taller-Fade/Taller-Fade.ino
@@ -0,0 +1,34 @@
+/** @file    Taller-Fade.h
+ *  @author  Jorge Villalobos Carvajal (jorgemvc@gmail.com)
+ *  @date    07.Abr.2018  
+ *  @version 1.0 
+ *  
+ *  @brief   Lectura de un sensor ultrasonico
+ *
+ *  @licence  MIT License
+ */
+int led = 3;           // Puerto PWM del led
+int brightness = 0;    // Brillo del led [0-255]
+int fadeAmount = 5;    // Incremento del brillo
+
+void setup() {
+  // ---- Declara el puerto del led como salida
+  pinMode(led, OUTPUT);
+}  // void setup
+
+void loop() {
+  // ---- Se establece el brillo del led
+  analogWrite(led, brightness);
+
+  // ---- Se incrementa el brillo
+  brightness += fadeAmount;
+
+  // ---- Invierte el incremento si llega a algún
+  //      límite de los valores posibles
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+
+  // Espera 100 milisegundos
+  delay(100);
+}  // void setup
